22033127bacc0e04de1575e9e7b2fb6ea9320e6c
Russian-Idiot <ranil2086@bk.ru>
2023-01-29 18:27:13 +0500

    initial state

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-import express from 'express';
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://Ranil2086:Ranil2086@cluster0.p8zfpev.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log("Error with connecting to DB", err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("12");
+app.get("/", (_, res) => {
+  res.send("12");
+});
+
+app.post("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Noname",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4444, (err) => {
-    if (err ) {
-        return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log("Server is running correct");
-})
+  console.log("Server is running correct");
+});
